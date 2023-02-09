dedc0d1eeadd4535f1ec87b3f7c66ac3f2e142e1
Dan <retrotechgaming@gmail.com>
2023-02-08 19:39:04 -0500

    Began setting up connection.js and added .env.EXAMPLE file within root directory

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlesbars');
+const exphbs = require('express-handlebars');
 const routes = ('./controllers');
 const helpers = require('./utils/helpers');
 
