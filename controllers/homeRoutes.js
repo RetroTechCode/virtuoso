da705f67ad5ffe602bd8853adfea76e4a2cca09f
Josh <lutz_143@yahoo.com>
2023-02-15 09:02:17 -0500

    added functionality for a visitor to click on a band's post and be routed to that band's profile page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,10 +17,6 @@ router.get('/', withAuth, async (req, res) => {
             },
           ]
         },
-        // {
-        // model: Stats,
-        // attributes: [['id', 'stats_id'], 'month_date', 'monthly_spotify_listeners', 'monthly_avg_stage_time']
-        // }
       ]
     });
 
@@ -37,81 +33,6 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
-// router.get('/posts/:id', withAuth, async (req, res) => {
-//   try {
-//     const postData = await Post.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: Comment,
-//           attributes: [['id','comment_id_test'], 'comment', 'post_id', 'user_id'],
-//           include: {
-//             model: User,
-//             attributes: ['username', 'github']
-//           }
-//         },
-//         {
-//         model: User,
-//         attributes: ['username', 'github']
-//         }
-//       ],
-//     });
-//     const post = postData.get({ plain: true });
-
-//     res.render('editPost', {
-//       ...post,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
-
-// router.get('/comments/:id', withAuth, async (req, res) => {
-//   try {
-//     const postData = await Post.findByPk(req.params.id, {
-//       attributes: [
-//         ['id', 'post_id'], 'title', 'date_created', 'post_content', ['user_id', 'post_user_id']
-//       ],
-//       include: [
-//         {
-//           model: Comment,
-//           attributes: ['id', 'comment', 'comment_date','user_id'],
-//           include: {
-//             model: User,
-//             attributes: ['username', 'github']
-//           }
-//         },
-//         {
-//         model: User,
-//         attributes: ['username', 'github']
-//         }
-//       ],
-//     });
-    
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//     });
-
-//     const post = postData.get({ plain: true });
-//     const user = userData.get({ plain: true });
-
-//     res.render('singlePost', {
-//       ...post,
-//       ...user,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
-
-
 // Use withAuth middleware to prevent access to route
 router.get('/band-profile', withAuth, async (req, res) => {
   try {
@@ -123,14 +44,8 @@ router.get('/band-profile', withAuth, async (req, res) => {
         include: [
           {
           model: Band,
-          attributes: ['username', 'band_name', 'email', 'manager_name']
+          attributes: [['id', 'band_id'], 'username', 'band_name', 'email', 'manager_name']
           },
-          // { model: Comment,
-          // attributes: ['comment', 'comment', 'comment_date', 'user_id'],
-          // include: {
-          //   model: User,
-          //   attributes: ['username', 'github']
-          // }}
           ]}],
     });
 
@@ -139,6 +54,30 @@ router.get('/band-profile', withAuth, async (req, res) => {
     res.render('band-profile', {
       ...band,
       logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('bands/:id', async (req, res) => {
+  try {
+    const bandData = await Band.findByPk(req.params.id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post,
+        attributes: ['title', 'post_content', 'date_created'],
+        include: [
+          {
+          model: Band,
+          attributes: [['id', 'band_id'], 'username', 'band_name', 'email', 'manager_name']
+          },
+          ]}],
+    });
+    const band = bandData.get({ plain: true });
+
+    res.render('band', {
+      ...band,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
